310f9058daf93961d419f5dc6198768be3a86a77
Alexander Yakimainen <Alexander@Yakimainen.mail>
2023-02-19 11:58:42 +0300

    Commit after reset

--- src/main/java/RandomNumbers.java
+++ src/main/java/RandomNumbers.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public final class RandomNumbers {
     static int getRandomNumber(int low, int high)  {
         if (high - low <=0 ){
-            throw new IllegalArgumentException("bound must be positive");
+            throw new IllegalArgumentException("Bound must be positive");
         }
         Random rand = new Random();
         return rand.nextInt(high - low + 1) + low;
